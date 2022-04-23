f6bea0e3cd64fef30e0ba533bf008cefb9f26f8b
semsmirn <semen.smirnov@opencascade.com>
2022-04-23 20:10:07 +0300

    [OBCCT-3] Implemented multythreading functinality

--- SocketTcpServer/SocketTcpServer/Program.cs
+++ SocketTcpServer/SocketTcpServer/Program.cs
@@ -2,87 +2,102 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Net;
 using System.Net.Sockets;
 
 
 namespace SocketTcpServer
 {
-    class Program
+
+  class myThread
+  {
+    Thread thread;
+    public myThread(string name, Socket theListeningSocket)
     {
-        static int port = 8005; // порт для приема входящих запросов
-        static void Main(string[] args)
-        {
-            String Host = Dns.GetHostName();
-            Console.WriteLine("Comp name = " + Host);
-            IPAddress[] IPs;
-            IPs = Dns.GetHostAddresses(Host);
-            foreach (IPAddress ip1 in IPs)
-                Console.WriteLine(ip1);
-
-
-            //получаем адреса для запуска сокета
-            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
-
-            // создаем сокет сервера
-            Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            try
-            {
-                // связываем сокет с локальной точкой, по которой будем принимать данные
-                listenSocket.Bind(ipPoint);
-
-                // начинаем прослушивание
-                listenSocket.Listen(10);
-
-                Console.WriteLine("Сервер запущен. Ожидание подключений...");
-
-                // готовимся  получать  сообщение
-                StringBuilder builder = new StringBuilder("");
-                int bytes = 0; // количество полученных байтов за 1 раз
-                int kol_bytes = 0;//количество полученных байтов
-                byte[] data = new byte[255]; // буфер для получаемых данных
-
-                Socket handler = listenSocket.Accept();
-
-                while (true)
-                {
-                    if (builder.ToString() == "shutdown")
-                    {
-                        handler = listenSocket.Accept();  // сокет для связи с     клиентом
-                    }
-
-                    builder = new StringBuilder("");
-
-                    do
-                    {
-                        bytes = handler.Receive(data);  // получаем сообщение
-                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
-                        kol_bytes += bytes;
-                    }
-                    while (handler.Available > 0);
-
-                    Console.WriteLine(DateTime.Now.ToShortTimeString() + ": " + builder.ToString());
-                    Console.WriteLine(kol_bytes + "bytes\n");
-                    // отправляем ответ
-                    string message = "message was send";
-
-                    byte[] aBytes = Encoding.UTF8.GetBytes(message);
-
-                    handler.Send(aBytes);
-                    // закрываем сокет
-
-                    if (builder.ToString() == "shutdown")
-                    {
-                        handler.Shutdown(SocketShutdown.Both);
-                        handler.Close();
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-        }
+      thread = new Thread(this.func);
+      thread.Name = name;
+      thread.Start(theListeningSocket);
     }
 
+    void func(object listeningSocket)
+    {
+
+      Socket listenSocket = listeningSocket as Socket;
+
+      StringBuilder builder = new StringBuilder("");
+      int bytes = 0;
+      int kol_bytes = 0;
+      byte[] data = new byte[255];
+
+      Socket handler = listenSocket.Accept();
+
+      while (true)
+      {
+        builder = new StringBuilder("");
+
+        do
+        {
+          bytes = handler.Receive(data);  // получаем сообщение
+          builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+          kol_bytes += bytes;
+        }
+        while (handler.Available > 0);
+
+        Console.WriteLine(DateTime.Now.ToShortTimeString() + ": " + builder.ToString());
+        Console.WriteLine(kol_bytes + "bytes\n");
+        // отправляем ответ
+        string message = "message was send";
+
+        byte[] aBytes = Encoding.UTF8.GetBytes(message);
+
+        handler.Send(aBytes);
+
+        if (builder.ToString() == "shutdown")
+        {
+          handler.Shutdown(SocketShutdown.Both);
+          handler.Close();
+          break;
+        }
+      }
+    }
+
+    class Program
+    {
+      static int port = 8005; // порт для приема входящих запросов
+      static void Main(string[] args)
+      {
+        String Host = Dns.GetHostName();
+        Console.WriteLine("Comp name = " + Host);
+        IPAddress[] IPs;
+        IPs = Dns.GetHostAddresses(Host);
+        foreach (IPAddress ip1 in IPs)
+          Console.WriteLine(ip1);
+
+
+        //получаем адреса для запуска сокета
+        IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+
+        // создаем сокет сервера
+        Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        try
+        {
+          // связываем сокет с локальной точкой, по которой будем принимать данные
+          listenSocket.Bind(ipPoint);
+
+          // начинаем прослушивание
+          listenSocket.Listen(10);
+
+          for (; ; )
+          {
+            myThread aMyThread = new myThread("sa", listenSocket);
+          }
+        }
+        catch (Exception ex)
+        {
+          Console.WriteLine(ex.Message);
+        }
+      }
+    }
+  }
 }
